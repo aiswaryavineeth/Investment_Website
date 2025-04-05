9b841803ae471998a8c09d3c6a866cff359595e8
aiswaryavin <aiswaryakrishnan100@gmail.com>
2025-04-05 15:25:31 +0400

    login

--- src/test/java/Tests/LoginTest.java
+++ src/test/java/Tests/LoginTest.java
@@ -1,4 +1,24 @@
 package Tests;
 
+import POM.LoginPage;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static core.Main.driver;
+import static core.Main.initializeDriver;
+
 public class LoginTest {
+    @BeforeClass
+    public void setup(){
+        initializeDriver();
+    }
+    @Test(priorty = 2)
+    public void logintest() throws InterruptedException{}{
+        LoginPage loginPage= new LoginPage(driver);
+        loginPage.openLoginPage("https://aqaryaid.com/en");
+        loginPage.closepopup();
+        loginPage.clickLoginButton();
+
+    }
+
 }
--- src/test/java/core/Main.java
+++ src/test/java/core/Main.java
@@ -1,4 +1,24 @@
 package core;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.IOException;
+import java.time.Duration;
+
 public class Main {
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+    public static void initializeDriver(){
+        driver= new ChromeDriver();
+        wait= new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+    public static  void quitDriver() throws IOException {
+        if(driver!=null){
+            driver.quit();
+        }
+        String projectPath ="C:\\Users\\Aiswarya\\IdeaProjects\\Investment_Website";
+        String commnand="cmd /c start cmd.exe /K \"cd /d " + projectPath + " && allure serve allure-results\"";
+    }
 }
